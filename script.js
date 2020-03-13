21be2daedf54b3226c1a787e5ecbb4158fcd07fa
ornel77 <t.ornel@yahoo.fr>
2020-03-13 22:27:51 +0100

    adding queries and form validation ok DONE

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ form.addEventListener('submit', (e) => {
     let inputValue = inputField.value
     
     let mailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
-    
+
     if(inputValue === '' ||  !mailRegex.test(inputValue))  {
         errMsg.style.display = 'block'
         iconError.style.display = 'block'
